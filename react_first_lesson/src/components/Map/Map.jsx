f79f8cc0efa72a56ec1088c26365c24baefeffbc
bunsbof <nguyenminhcuong2000@gmail.com>
2022-08-14 16:21:53 +0700

    #2022/08/14 cuong - Add Location Suggestor Project

---
+++ react_first_lesson/src/components/Map/Map.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import { Rating } from "@material-ui/lab";
+
+import useStyles from "./styles";
+
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBGhmhrE2T3jPhuzh1hSCSRmhmlWGZTwQA" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ne: e.marginBounds.ne, sw: e.margin.sw})
+        }}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
