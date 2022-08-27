1d61c5fdc5e07398a096cad5a09f7c42dc18ad31
The <nguyenminhcuong2000@gmail.com>
2022-08-28 01:52:31 +0700

    #202208/28 cuong - Create Videos component

--- youtube_clone/src/components/Feed.jsx
+++ youtube_clone/src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import {Sidebar, Videos} from "./";
 
 const Feed = () => {
   return (
@@ -16,6 +16,12 @@ const Feed = () => {
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2022 Bunsbof Media
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          New<span style={{ color: '#F31503' }}>Videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
