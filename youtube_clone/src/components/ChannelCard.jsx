91ebf2b5a73b44c2356c9f4f1e89a297c2318d39
The <nguyenminhcuong2000@gmail.com>
2022-08-28 22:15:31 +0700

    #2022/08/28 cuong - Fix bug for ChannelCard & ChannelDetail components

--- youtube_clone/src/components/ChannelCard.jsx
+++ youtube_clone/src/components/ChannelCard.jsx
@@ -1,10 +1,23 @@
+import React from "react";
 import { Box, CardContent, CardMedia, Typography } from "@mui/material";
-import { CheckCircle } from "@mui/icons-material";
-import { Link } from "@mui/icons-material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail }) => (
-  <Box sx={{ boxShadow: "none", borderRadius: "20px" }}>
+const ChannelCard = ({ channelDetail, marginTop }) => (
+  <Box
+    sx={{
+      boxShadow: "none",
+      borderRadius: "20px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: { xs: "356px", md: "320px" },
+      height: "326px",
+      margin: "auto",
+      marginTop,
+    }}
+  >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
       <CardContent
         sx={{
@@ -20,8 +33,28 @@ const ChannelCard = ({ channelDetail }) => (
             channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture
           }
           alt={channelDetail?.snippet?.title}
-          sx={{ borderRadius: "50%", height: "180px", width: "180px" }}
-        ></CardMedia>
+          sx={{
+            borderRadius: "50%",
+            height: "180px",
+            width: "180px",
+            mb: 2,
+            border: "1px solid #e3e3e3",
+          }}
+        />
+        <Typography variant="h6">
+          {channelDetail?.snippet?.title}{" "}
+          <CheckCircleIcon
+            sx={{ fontSize: "14px", color: "gray", ml: "5px" }}
+          />
+        </Typography>
+        {channelDetail?.statistics?.subscriberCount && (
+          <Typography sx={{ fontSize: "15px", fontWeight: 500, color: "gray" }}>
+            {parseInt(
+              channelDetail?.statistics?.subscriberCount
+            ).toLocaleString("en-US")}{" "}
+            Subscribers
+          </Typography>
+        )}
       </CardContent>
     </Link>
   </Box>
