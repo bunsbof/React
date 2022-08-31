c84779b986a4a89240884591460061c4606fde78
The <nguyenminhcuong2000@gmail.com>
2022-08-31 22:38:27 +0700

    #2022/08/31 cuong - config SearchFeed & ChannelDetail components

--- youtube_clone/src/components/SearchFeed.jsx
+++ youtube_clone/src/components/SearchFeed.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
 
-export default SearchFeed
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, []);
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        {selectedCategory}
+        <span style={{ color: "#F31503" }}> Videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
