d1695cce6430a49b4990fb08fd353867bf3f2873
The Untitled <nguyenminhcuong2000@gmail.com>
2022-09-17 16:01:30 +0700

    #2022/09/17 cuong - learn useEffect hook

---
+++ use-effect/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([])
+  
+  useEffect(() => {
+    console.log('resource changed');
+
+    return () => {
+      console.log('return from resource change');
+    }
+  }, [resourceType]);
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+    </>
+  );
+}
