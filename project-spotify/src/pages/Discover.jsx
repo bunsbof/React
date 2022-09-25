1e60ad7193a9087000851bf19bd8c3fb0501f734
The <nguyenminhcuong2000@gmail.com>
2022-09-25 18:58:13 +0700

    #2022/09/25 cuong - Done TopPlay component

--- project-spotify/src/pages/Discover.jsx
+++ project-spotify/src/pages/Discover.jsx
@@ -1,9 +1,14 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { HiCake } from "react-icons/hi";
 
 const Discover = () => {
+  const dispatch = useDispatch()
+  const {activeSong, isPlaying} = useSelector((state) => state.player)
   const { data, isFetching, error } = useGetTopChartsQuery();
 
   const genreTitle = "Pop";
@@ -32,7 +37,7 @@ const Discover = () => {
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
-          <SongCard key={song} song={song} i={i} />
+          <SongCard key={song} song={song} i={i} isPlaying={isPlaying} activeSong={activeSong} data={data} />
         ))}
       </div>
     </div>
